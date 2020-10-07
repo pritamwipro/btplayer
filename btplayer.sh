ce2339dfa2358d06d8158436347dc8ebe08ae34c
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-07 12:28:19 +0100

    add git clone cmd

--- btplayer.sh
+++ btplayer.sh
@@ -9,27 +9,16 @@ fi
 
 cd "${btplayerdir}" || { printf "cd failed, exiting\n" >&2;  return 1; }
 
-youview="https://github.com/pritamwipro/kth.git"
+youview_btplayer="https://github.com/pritamwipro/kth.git"
 
 # printf "Gitsource: "
 # read -r gitsource
 
-git clone "${youview}"
+git clone "${youview_btplayer}"
 
 # unset gitsource
 
 echo "Please choose from the options bellow"
 
-echo "1) Go back to your working directory"
-echo "2) Go to the 'git-sources' folder"
-
-read -r ans
-back="1"
-stay="2"
-if [ "$ans" = "$back" ]; then
-      cd - || { printf "cd failed, exiting\n" >&2; unset ans; return 1; }
-elif [ "$ans" = "$stay" ]; then
-      cd "${btplayerdir}" || { printf "cd failed, exiting\n" >&2; unset ans; return 1; }
-fi
 
 unset ans
