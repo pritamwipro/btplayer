b35e5e3b1b18f8279f8a028484b182d78879b0cb
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-07 14:37:56 +0100

    add git clone cmd

--- btplayer.sh
+++ btplayer.sh
@@ -2,6 +2,7 @@
 
 btplayerdir=$1
 
+# Check directory exist or not, if not exists then create
 echo "Check ${btplayerdir}"
 if [ ! -d "${btplayerdir}" ]; then
     mkdir "${btplayerdir}"
@@ -9,22 +10,24 @@ fi
 
 cd "${btplayerdir}" || { printf "cd failed, exiting\n" >&2;  return 1; }
 
-inlife_puppet="https://gitlab.tools.btcsp.co.uk/tv/inlife_puppet.git"
+# Clone youview-btplayer
 youview_btplayer="https://gitlab.tools.btcsp.co.uk/tv/youview-btplayer.git"
-
-git clone "${inlife_puppet}"
 git clone "${youview_btplayer}"
 
-echo "Go to the ${btplayerdir}/inlife_puppet directory"
+# Clone inlife_puppet
+inlife_puppet="https://gitlab.tools.btcsp.co.uk/tv/inlife_puppet.git"
+git clone "${inlife_puppet}"
+
+# Go to the ${btplayerdir}/inlife_puppet directory
 cd "${btplayerdir}"/inlife_puppet
 
-echo "Pull vosp_sim and cms_static_content from git by using ./doGitPullAll.sh"
+# Pull vosp_sim and cms_static_content from git by using ./doGitPullAll.sh
 ./doGitPullAll.sh
 
-echo "Go back to the youview-btplayer directory"
+# Go back to the youview-btplayer directory
 cd ../youview-btplayer
 
-echo "submodule init and update for youview"
+# submodule init and update for youview
 git submodule init
 git submodule update
 
