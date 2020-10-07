5bc85e36f804bc34145e0d561542500942dc04da
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-07 08:48:58 +0100

    first commit

---
+++ brew.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install node
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install skype
+
+# Install Source Code Pro Font
+# brew tap homebrew/cask-fonts
+# brew cask install font-source-code-pro
+
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+############################
+# This script creates symlinks from the home directory to any desired btplayerfiles in ${homedir}/btplayer
+# And also installs Homebrew Packages
+# And sets BT Player preferences
+############################
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+# btplayerfiles directory
+btplayerdir=${homedir}/btplayerfiles
+
+# list of files/folders to symlink in ${homedir}
+files="private"
+
+# change to the btplayerfiles directory
+echo "Changing to the ${btplayerdir} directory"
+cd ${btplayerdir}
+echo "...done"
+
+# create symlinks (will overwrite old btplayerfiles)
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${btplayerdir}/.${file} ${homedir}/.${file}
+done
+
+# Download Git Auto-Completion
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ${homedir}/.git-completion.bash
+
+# Run the Homebrew Script
+./brew.sh
