9ee426c2d08f87fa6f7b45b67bf5ff8312ab4b83
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-07 12:10:37 +0100

    add git clone cmd

--- btplayer.sh
+++ btplayer.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
-echo "NEW to the $1 directory"
 
-if [ ! -d "$1" ]; then
-    mkdir "$1"
+$btplayerdir=$1
+
+if [ ! -d "$btplayerdir" ]; then
+    mkdir "$btplayerdir"
 fi
 
-cd "$1" || { printf "cd failed, exiting\n" >&2;  return 1; }
+cd "$btplayerdir" || { printf "cd failed, exiting\n" >&2;  return 1; }
+
+gitsource="https://gitlab.tools.btcsp.co.uk/tv/youview-btplayer.git"
+
+printf "Gitsource: "
+read -r gitsource
+
+git clone "$gitsource"
+
+unset gitsource
+
+echo "Please choose from the options bellow"
+
+echo "1) Go back to your working directory"
+echo "2) Go to the 'git-sources' folder"
+
+read -r ans
+back="1"
+stay="2"
+if [ "$ans" = "$back" ]; then
+      cd - || { printf "cd failed, exiting\n" >&2; unset ans; return 1; }
+elif [ "$ans" = "$stay" ]; then
+      cd "$btplayerdir" || { printf "cd failed, exiting\n" >&2; unset ans; return 1; }
+fi
+
+unset ans
