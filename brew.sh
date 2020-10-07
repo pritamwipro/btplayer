bf21917cbaac928d5a1cff51ae5b5524ecff1539
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-07 11:15:15 +0100

    add git clone cmd

--- brew.sh
+++ brew.sh
@@ -2,15 +2,15 @@
 
 # Install Brew Packages
 brew install git
-# brew install node
-# brew install tree
+brew install node
+brew install tree
 
 # Install MacOS Applications
 brew cask install google-chrome
-# brew cask install firefox
-# brew cask install visual-studio-code
-# brew cask install virtualbox
-# brew cask install vagrant
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install virtualbox
+brew cask install vagrant
 brew cask install skype
 
 # Install Source Code Pro Font
